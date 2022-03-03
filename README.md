e3b6785b7ae66027b5d645a835bab7a203c17106
Jack Lu <jacklu@jacklu.me>
2022-03-04 01:47:21 +0800

    +filter

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ var WechatyInfoPlugin = require('wechaty-info');
 var bot = new Wechaty();
 bot.use(
 	WechatyInfoPlugin({
+		filter: [{ contact: { name: '小红' } }, { room: { topic: /^都是老师/ } }],
 		command: "讲个笑话",
 		fetch: () => axios.get("https://api.apiopen.top/getJoke?type=text&count=1")
 			.then(({ data: data }) => data.result[0].text, () => "请求失败"),
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 /** @typedef { import("wechaty").Wechaty } Wechaty */
 /** @typedef { import("wechaty").Message } Message */
+/** @typedef { import("wechaty").Sayable } Sayable */
+/** @typedef { import("wechaty-puppet").ContactQueryFilter } ContactQueryFilter */
+/** @typedef { import("wechaty-puppet").RoomQueryFilter } RoomQueryFilter */
+/** @typedef {{ contact: ContactQueryFilter } | { room: RoomQueryFilter }} SayableQueryFilter */
 
 /**
  * @param {Object} config
+ * @param {SayableQueryFilter[]} [config.filter] the filter of contacts and rooms to enable this plugin
  * @param {string} config.command the message text to ask for the information
  * @param {() => Promise<string>} config.fetch the information fetcher, fulfills with information and rejects with error message
  * @param {Object} config.throttle the throttle config
@@ -14,7 +19,22 @@ module.exports = function WechatyInfoPlugin(config) {
 		// throttle, based on https://stackoverflow.com/a/27078401/4127811
 		var waiting = false;
 		bot.on("message", async (/** @type {Message} */message) => {
-			if (message.text() == config.command)
+			if (
+				message.text() == config.command
+				&& (
+					!config.filter || await (
+						async from => (
+							await Promise.all(
+								config.filter.map(
+									filter => sayableQueryFilterFactory(filter)(from)
+								)
+							)
+						).some(Boolean)
+					)(
+						message.room() || message.talker()
+					)
+				)
+			)
 				if (!waiting) {
 					try { var information = await config.fetch(); }
 					catch (/** @type {string} */e) { var error = e; }
@@ -30,5 +50,18 @@ module.exports = function WechatyInfoPlugin(config) {
 						await receiver.say(config.throttle?.message);
 					}
 		});
+		function sayableQueryFilterFactory(/** @type {SayableQueryFilter} */filter) {
+			return async function (/** @type {Sayable} */sayable) {
+				if (filter.contact)
+					return bot.puppet.contactQueryFilterFactory(filter.contact)(
+						await bot.puppet.contactPayload(sayable.id)
+					);
+				if (filter.room)
+					return bot.puppet.roomQueryFilterFactory(filter.room)(
+						await bot.puppet.roomPayload(sayable.id)
+					);
+				return false;
+			};
+		}
 	};
 };
