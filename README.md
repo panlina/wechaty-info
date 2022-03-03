8566d16eacaa6c3c60492bbf9ae33ef3204b1add
Jack Lu <jacklu@jacklu.me>
2022-03-03 11:35:04 +0800

    +throttle

--- README.md
+++ README.md
@@ -10,7 +10,11 @@ var WechatyInfoPlugin = require('wechaty-info');
 var bot = new Wechaty({
 	command: "讲个笑话",
 	fetch: () => axios.get("https://api.apiopen.top/getJoke?type=text&count=1")
-		.then(({ data: data }) => data.result[0].text, () => "请求失败")
+		.then(({ data: data }) => data.result[0].text, () => "请求失败"),
+	throttle: {
+		timeout: 60 * 1000,
+		message: "对不起，1分钟内只能看一个笑话[呲牙]"
+	}
 });
 bot.use(WechatyInfoPlugin());
 ```
--- index.js
+++ index.js
@@ -5,16 +5,30 @@
  * @param {Object} config
  * @param {string} config.command the message text to ask for the information
  * @param {() => Promise<string>} config.fetch the information fetcher, fulfills with information and rejects with error message
+ * @param {Object} config.throttle the throttle config
+ * @param {number} config.throttle.timeout the timeout in milliseconds
+ * @param {string} config.throttle.message the message to reply when throttled
  */
 module.exports = function WechatyInfoPlugin(config) {
 	return function (/** @type {Wechaty} */bot) {
+		// throttle, based on https://stackoverflow.com/a/27078401/4127811
+		var waiting = false;
 		bot.on("message", async (/** @type {Message} */message) => {
-			if (message.text() == config.command) {
-				try { var information = await config.fetch(); }
-				catch (/** @type {string} */e) { var error = e; }
-				var receiver = message.room() || message.talker();
-				await receiver.say(information || error);
-			}
+			if (message.text() == config.command)
+				if (!waiting) {
+					try { var information = await config.fetch(); }
+					catch (/** @type {string} */e) { var error = e; }
+					var receiver = message.room() || message.talker();
+					await receiver.say(information || error);
+					waiting = true;
+					setTimeout(function () {
+						waiting = false;
+					}, config.throttle?.timeout);
+				} else
+					if (config.throttle?.message) {
+						var receiver = message.room() || message.talker();
+						await receiver.say(config.throttle?.message);
+					}
 		});
 	};
 };
